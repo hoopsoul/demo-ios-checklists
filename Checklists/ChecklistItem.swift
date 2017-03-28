3ef188d18ba23b2e73f58f31f147a40dd68e9fce
Qiang <fuqiang@outlook.com>
2017-03-29 00:11:17 +0800

    first commit

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,82 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Qiang on 01/03/2017.
+//  Copyright © 2017 Qiang. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class ChecklistItem: NSObject, NSCoding {
+    var text = ""
+    var checked = false
+    var dueDate = Date()
+    var shouldRemind = false
+    var itemID: Int
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(text, forKey: "Text")
+        aCoder.encode(checked, forKey: "Checked")
+        aCoder.encode(dueDate, forKey: "DueDate")
+        aCoder.encode(shouldRemind, forKey: "ShouldRemind")
+        aCoder.encode(itemID, forKey: "ItemID")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        text = aDecoder.decodeObject(forKey: "Text") as! String
+        checked = aDecoder.decodeBool(forKey: "Checked")
+        dueDate = aDecoder.decodeObject(forKey: "DueDate") as! Date
+        shouldRemind = aDecoder.decodeBool(forKey: "ShouldRemind")
+        itemID = aDecoder.decodeInteger(forKey: "ItemID")
+        
+        super.init()
+    }
+    
+    override init() {
+        itemID = DataModel.nextChecklistItemID()
+        super.init()
+    }
+    
+    init(text: String, checked: Bool) {
+        self.text = text
+        self.checked = checked
+        self.itemID = DataModel.nextChecklistItemID()
+        super.init()
+    }
+    
+    func scheduleNotification() {
+        removeNotification()
+        if shouldRemind && dueDate > Date() {
+            let content = UNMutableNotificationContent()
+            content.title = "Reminder:"
+            content.body = text
+            content.sound = UNNotificationSound.default()
+            
+            let calendar = Calendar(identifier: .gregorian)
+            let components = calendar.dateComponents([.month, .day, .hour, .minute], from: dueDate)
+            
+            let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+            let request = UNNotificationRequest(identifier: "\(itemID)", content: content, trigger: trigger)
+            
+            let center = UNUserNotificationCenter.current()
+            center.add(request)
+            
+            print("We should schedule a notification \(request) for item \(itemID)")
+        }
+    }
+    
+    func removeNotification() {
+        let center = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: ["\(itemID)"])
+    }
+    
+    deinit {
+        removeNotification()
+    }
+}
---
+++ Checklists/ListDetailViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ListDetailViewController.swift
+//  Checklists
+//
+//  Created by Qiang on 14/03/2017.
+//  Copyright © 2017 Qiang. All rights reserved.
+//
+
+import UIKit
+
+protocol ListDetailViewControllerDelegate: class {
+    func listDetailViewControllerDidCancel(_ controller: ListDetailViewController)
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishAdding checklist: Checklist)
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist)
+}
+
+class ListDetailViewController: UITableViewController, UITextFieldDelegate, IconPickerViewControllerDelegate {
+    @IBOutlet weak var textFiled: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var iconImageView: UIImageView!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    var checklistToEdit: Checklist?
+    var iconName = "Folder"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textFiled.text = checklist.name
+            doneBarButton.isEnabled = true
+            iconName = checklist.iconName
+        }
+        iconImageView.image = UIImage(named: iconName)
+    }
+    
+    func iconPicker(_ picker: IconPickerViewController, didPick iconName: String) {
+        self.iconName = iconName
+        iconImageView.image = UIImage(named: iconName)
+        let _ = navigationController?.popViewController(animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PickIcon" {
+            let controller = segue.destination as! IconPickerViewController
+            controller.delegate = self
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textFiled.becomeFirstResponder()
+    }
+    
+    @IBAction func cancel() {
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    @IBAction func done() {
+        if let checklist = checklistToEdit {
+            checklist.name = textFiled.text!
+            checklist.iconName = iconName
+            delegate?.listDetailViewController(self, didFinishEditing: checklist)
+        } else {
+            let checklist = Checklist(name: textFiled.text!, iconName: iconName)
+            delegate?.listDetailViewController(self, didFinishAdding: checklist)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if indexPath.section == 1 {
+            return indexPath
+        } else {
+            return nil
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textFiled.text! as NSString
+        let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+        doneBarButton.isEnabled = (newText.length > 0)
+        return true
+    }
+}
